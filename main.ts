0cc1f804fb913d8b5f18b4648e82be18c03d034f
gmteacher2018 <39750192+gmteacher2018@users.noreply.github.com>
2018-11-30 23:35:52 -0500

    exported smiles function

--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
-function smiles() {
-    basic.showLeds(`
+
+
+//% color="#AA278D"
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+
+    export function smiles() {
+        basic.showLeds(`
         # . . . #
         . . . . .
         # . . . #
         . # . # .
         . . # . .
         `)
-    basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.No)
+    }
+
 }
